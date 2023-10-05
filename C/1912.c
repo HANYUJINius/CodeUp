96784ee8995fc949b71d282e8d6edca12d33fa81
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-10-05 23:36:40 +0900

    (재귀함수) 팩토리얼 계산

---
+++ C/1912.c
@@ -0,0 +1,21 @@
+//CodeUp_c_1912.c
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if(n == 0)
+        return 1;
+    else
+        return n * factorial(n-1);
+}
+
+int main(void)
+{
+    int num;
+    
+    scanf("%d", &num);
+    printf("%d", factorial(num));
+    
+    return 0;
+}
