df6f953bf870079b46df774c189f4f5d607fef80
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-10-07 13:10:44 +0900

    Update 1928.c / (재귀함수) 우박수 <3n+1>

--- C/1928.c
+++ C/1928.c
@@ -4,22 +4,13 @@
 
 void numPrint(int n)
 {
-    //printf("%d\n", n);
+    printf("%d\n", n);
     if (n == 1)
-    {
-        printf("%d\n", n);
         return;
-    }
     else if (n % 2 == 0)
-    {
-        printf("%d\n", n);
         return numPrint(n / 2);
-    }
     else
-    {
-        printf("%d\n", n);
         return numPrint(3 * n + 1);
-    }
 }
 
 int main(void)
