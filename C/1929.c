bb9d2f0aed3782748c9d303a5e60548084fe6ba9
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-10-08 20:59:33 +0900

    (재귀함수) 우박수 <3n+1>

---
+++ C/1929.c
@@ -0,0 +1,25 @@
+//CodeUp_c_1929.c
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void numPrint(int n)
+{
+    if (n == 1)
+    {
+        printf("%d\n", n);
+        return;
+    }
+    else if (n % 2 == 0)
+        numPrint(n / 2);
+    else
+        numPrint(n * 3 + 1);
+    printf("%d\n", n);
+}
+
+int main(void)
+{
+    int num;
+    scanf("%d", &num);
+    numPrint(num);
+    return 0;
+}
